ef77ff7fde4df185ffdaa29e9ee7967f5e827cdc
Patrick Styll <patrick.styll@tuwien.ac.at>
2024-08-19 16:47:42 +0900

    remove redundant code

--- project/module/models/swiftbert.py
+++ project/module/models/swiftbert.py
@@ -1,16 +1,3 @@
-# https://github.com/Project-MONAI/MONAI/blob/dev/monai/networks/nets/swin_unetr.py
-
-# Copyright (c) MONAI Consortium
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#     http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from __future__ import annotations
 
 from collections.abc import Sequence
@@ -32,11 +19,6 @@ __all__ = [
 ]
 
 class SwiFTBERT(nn.Module):
-    """
-    Swin UNETR based on: "Hatamizadeh et al.,
-    Swin UNETR: Swin Transformers for Semantic Segmentation of Brain Tumors in MRI Images
-    <https://arxiv.org/abs/2201.01266>"
-    """
 
     #patch_size: Final[int] = 2
 
