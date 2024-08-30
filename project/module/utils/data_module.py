3bcd1f60a9cc7f8ee55fb6bdc7bd9971544aae16
Patrick Styll <patrick.styll@tuwien.ac.at>
2024-08-30 23:29:07 +0900

    change sex to static value

--- project/module/utils/data_module.py
+++ project/module/utils/data_module.py
@@ -182,7 +182,7 @@ class fMRIDataModule(pl.LightningDataModule):
                 meta_task = meta_data[['Anger_smooth_05','Happy_smooth_05','Fear_smooth_05','Sad_smooth_05','Excited_smooth_05', 'Positive_smooth_05', 'Negative_smooth_05', 'frame']].dropna()
                             
                 for subject in os.listdir(img_root):
-                    sex = None # TODO check
+                    sex = 1 # TODO check
                     target = meta_task[['Anger_smooth_05', 'Happy_smooth_05', 'Fear_smooth_05', 'Sad_smooth_05', 'Excited_smooth_05', 'Positive_smooth_05', 'Negative_smooth_05']].values
                     target = target[np.argsort(meta_task['frame'].values)][4:] #skips first 4 frames
                     final_dict[str(subject)] = (sex, target)
