474edb9e26323316ab543e45555b7d8d63bfacda
Patrick Styll <patrick.styll@tuwien.ac.at>
2024-09-05 11:21:04 +0900

    change output heads

--- project/module/models/linear.py
+++ project/module/models/linear.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
 
-class LinearLayerReg(nn.Module):
+class LinearLayer(nn.Module):
     def __init__(self, input_dim, num_emotions):
-        super(LinearLayerReg, self).__init__()
+        super(LinearLayer, self).__init__()
         
         self.fc = nn.Linear(input_dim, num_emotions)
     
@@ -12,21 +12,5 @@ class LinearLayerReg(nn.Module):
         x = x.flatten(start_dim=1, end_dim=4).transpose(1, 2)
         
         output = self.fc(x)  # Shape: (b, t, num_emotions)
-        
-        return output
-
-class LinearLayerClf(nn.Module):
-    def __init__(self, input_dim, num_classes):
-        super(LinearLayerClf, self).__init__()
-        
-        self.fc = nn.Linear(input_dim, num_classes)
-    
-    def forward(self, x):
-        #(b, c, h, w, d, t) to (b, t, c*h*w*d)
-        x = x.flatten(start_dim=1, end_dim=4).transpose(1, 2)
-        
-        output = self.fc(x)  # Shape: (b, t, num_classes)
-        
-        #output = torch.sigmoid(output) # Shape: (b, t, num_classes), probabilities [0,1] for each class
         
         return output
