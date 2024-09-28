5ebebcb225fad96fdd2ed6a4571a7017db9bf27c
Patrick Styll <patrick.styll@tuwien.ac.at>
2024-09-28 16:15:43 +0200

    implemented and tested sliding windows with overlap

--- project/module/utils/data_preprocess_and_load/datasets.py
+++ project/module/utils/data_preprocess_and_load/datasets.py
@@ -235,7 +235,7 @@ class HBN(BaseDataset):
             if self.train and self.sliding_window_overlap:
                 for start_frame in range(0, session_duration, self.stride - self.sliding_window_overlap):
                     data_tuple = (i, subject_name, subject_path, start_frame, self.sample_duration, num_frames, target[start_frame:min(start_frame+self.sample_duration,num_frames)], sex)
-                    print(start_frame, min(start_frame+self.sample_duration,num_frames), target[start_frame:min(start_frame+self.sample_duration,num_frames)])
+                    #print(start_frame, min(start_frame+self.sample_duration,num_frames), target[start_frame:min(start_frame+self.sample_duration,num_frames)])
                     #print(num_frames, self.sample_duration, self.stride, session_duration)
                     data.append(data_tuple)
             else:
