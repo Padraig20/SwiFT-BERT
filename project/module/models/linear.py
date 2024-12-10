a3b07ab73502d9393bd81e9197dbd133c138ba14
Patrick Styll <patrick.styll@tuwien.ac.at>
2024-12-11 00:30:43 +0100

    fix temporal mapping layer

--- project/module/models/linear.py
+++ project/module/models/linear.py
@@ -2,20 +2,24 @@ import torch
 import torch.nn as nn
 
 class LinearLayer(nn.Module):
-    def __init__(self, input_dim, num_emotions, time_dim):
+    def __init__(self, input_dim, num_emotions, time_dim_orig, time_dim_patched):
         super(LinearLayer, self).__init__()
         
-        self.time_dim = time_dim
+        self.time_dim_patched = time_dim_patched
+        self.input_dim = input_dim
+        self.time_dim_orig = time_dim_orig
         
-        self.temporal_mapping = nn.Linear(time_dim, time_dim)
+        self.temporal_mapping = nn.Linear(time_dim_patched, time_dim_orig, bias=False)
         self.fc = nn.Linear(input_dim, num_emotions)
     
     def forward(self, x):
         #(b, c, h, w, d, t) to (b, t, c*h*w*d)
         x = x.flatten(start_dim=1, end_dim=4).transpose(1, 2)
         
-        if self.time_dim != x.shape[1]:
+        if self.time_dim_orig != x.shape[1]:
+            x = x.permute(0, 2, 1)
             x = self.temporal_mapping(x)
+            x = x.permute(0, 2, 1)
         
         output = self.fc(x)  # Shape: (b, t, num_emotions)
                 
