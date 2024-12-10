5a58b6dbeb577c8b173c5b52da643d8215ff7534
Patrick Styll <patrick.styll@tuwien.ac.at>
2024-12-10 23:16:11 +0100

    add target param to load_model

--- project/module/models/load_model.py
+++ project/module/models/load_model.py
@@ -49,7 +49,7 @@ def load_model(model_name, hparams=None):
     elif model_name == "mlp":
         net = SimpleMLP(dims, hparams.mlp_dim, hparams.target_dim)
     elif model_name == "linear":
-        net = LinearLayer(dims, hparams.target_dim)
+        net = LinearLayer(dims, hparams.target_dim, t)
     elif model_name == "lstm":
         net = LSTM(dims, hparams.lstm_dim, hparams.lstm_layers, hparams.target_dim)
     else:
